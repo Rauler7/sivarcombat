85f37210e35378297fbd134f79a7dc2feb46883e
vagrant <vagrant@kleer-csd-box>
2018-03-19 14:11:38 -0300

    Solucion del test luchadores

--- spec/luchador_spec.rb
+++ spec/luchador_spec.rb
@@ -7,4 +7,16 @@ describe "Combate" do
 		batalla.combate(2, 1).should == 1
 	end
 
+	it "Gana Jugador 2" do
+		batalla = Batalla.new
+		batalla.combate(1, 2).should == 2
+	end
+
+	it "Empate" do
+		batalla = Batalla.new
+		batalla.combate(1, 1).should == 0
+	end
+
+
+
 end
