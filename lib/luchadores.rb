d18675d32453dc2bc18844a9b4de30cb048dac67
rauler7 <rauler7@gmail.com>
2018-03-19 16:46:12 -0300

    Creacion de luchadores con sus atributos

--- lib/luchadores.rb
+++ lib/luchadores.rb
@@ -1,8 +1,57 @@
 require "./lib/random.rb"
 
-class Luchadores
-	def attack
-		r = Random.new
-		return r.primerAtaque
-	end
+class Luchador	
+	 def nayibB  
+		@nombre = "Nayib Bukele" 
+		@ataque = 80
+		@vida = 80
+		@precision = 70
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end
+
+	def milagroN  
+		@nombre = "Milagro Navas" 
+		@ataque = 30
+		@vida = 100
+		@precision = 50
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end 
+
+	def cipitio 
+		@nombre = "Cipitio" 
+		@ataque = 100
+		@vida = 50
+		@precision = 70
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end
+
+	def isabelR
+		@nombre = "Isabel Rodriguez" 
+		@ataque = 5
+		@vida = 200
+		@precision = 40
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end
+
+	def elProfe
+		@nombre = "Sanchez Ceren" 
+		@ataque = 3
+		@vida = 150
+		@precision = 20
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end 
+
+	def tacuazin
+		@nombre = "Norman Quijano" 
+		@ataque = 70
+		@vida = 60
+		@precision = 80
+		@imagen = ""
+		return [ @nombre, @ataque, @vida, @precision, @imagen]
+ 	end 
 end
